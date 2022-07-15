7f2055b6ce453db738dd20f57b940e8b8c388e62
tmerritt56 <104723943+tmerritt56@users.noreply.github.com>
2022-07-14 21:53:59 -0500

    added sve btn and local storage for the rest of the times.

--- Develop/Assets/script.js
+++ Develop/Assets/script.js
@@ -17,6 +17,13 @@ function displayTime() {
     eightA();
     nineA();
     tenA();
+    elevenA();
+    tweleveP();
+    oneP();
+    twoP();
+    threeP();
+    fourP();
+    fiveP();
     }
 
   function colorC () {
@@ -126,5 +133,110 @@ function displayTime() {
       localStorage.setItem("content2", input_textarea2.value);
 
       outputD2.textContent = input_textarea2.value;
+    }
+  }
+  function elevenA () {
+    var input_textarea3 = document.querySelector("#comment11am"); 
+    var outputD3 = document.querySelector("#comment11am");
+    var saveBtn3 = document.querySelector("#btn11am");
+
+    saveBtn3.addEventListener("click", updateOutput3);
+    outputD3.textContent = localStorage.getItem("content3");
+    input_textarea3.value = localStorage.getItem("content3");
+
+    function updateOutput3() {
+      localStorage.setItem("content3", input_textarea3.value);
+
+      outputD3.textContent = input_textarea3.value;
+    }
+  }
+  function tweleveP () {
+    var input_textarea4 = document.querySelector("#comment12pm"); 
+    var outputD4 = document.querySelector("#comment12pm");
+    var saveBtn4 = document.querySelector("#btn12pm");
+
+    saveBtn4.addEventListener("click", updateOutput4);
+    outputD4.textContent = localStorage.getItem("content4");
+    input_textarea4.value = localStorage.getItem("content4");
+
+    function updateOutput4() {
+      localStorage.setItem("content4", input_textarea4.value);
+
+      outputD4.textContent = input_textarea4.value;
+    }
+  }
+  function oneP () {
+    var input_textarea5 = document.querySelector("#comment1pm"); 
+    var outputD5 = document.querySelector("#comment1pm");
+    var saveBtn5 = document.querySelector("#btn1pm");
+
+    saveBtn5.addEventListener("click", updateOutput5);
+    outputD5.textContent = localStorage.getItem("content5");
+    input_textarea5.value = localStorage.getItem("content5");
+
+    function updateOutput5() {
+      localStorage.setItem("content5", input_textarea5.value);
+
+      outputD5.textContent = input_textarea5.value;
+    }
+  }
+  function twoP () {
+    var input_textarea6 = document.querySelector("#comment2pm"); 
+    var outputD6 = document.querySelector("#comment2pm");
+    var saveBtn6 = document.querySelector("#btn2pm");
+
+    saveBtn6.addEventListener("click", updateOutput6);
+    outputD6.textContent = localStorage.getItem("content6");
+    input_textarea6.value = localStorage.getItem("content6");
+
+    function updateOutput6() {
+      localStorage.setItem("content6", input_textarea6.value);
+
+      outputD6.textContent = input_textarea6.value;
+    }
+  }
+  function threeP () {
+    var input_textarea7 = document.querySelector("#comment3pm"); 
+    var outputD7 = document.querySelector("#comment3pm");
+    var saveBtn7 = document.querySelector("#btn3pm");
+
+    saveBtn7.addEventListener("click", updateOutput7);
+    outputD7.textContent = localStorage.getItem("content7");
+    input_textarea7.value = localStorage.getItem("content7");
+
+    function updateOutput7() {
+      localStorage.setItem("content7", input_textarea7.value);
+
+      outputD7.textContent = input_textarea7.value;
+    }
+  }
+  function fourP () {
+    var input_textarea8 = document.querySelector("#comment4pm"); 
+    var outputD8 = document.querySelector("#comment4pm");
+    var saveBtn8 = document.querySelector("#btn4pm");
+
+    saveBtn8.addEventListener("click", updateOutput8);
+    outputD8.textContent = localStorage.getItem("content8");
+    input_textarea8.value = localStorage.getItem("content8");
+
+    function updateOutput8() {
+      localStorage.setItem("content8", input_textarea8.value);
+
+      outputD8.textContent = input_textarea8.value;
+    }
+  }
+  function fiveP () {
+    var input_textarea9 = document.querySelector("#comment5pm"); 
+    var outputD9 = document.querySelector("#comment5pm");
+    var saveBtn9 = document.querySelector("#btn5pm");
+
+    saveBtn9.addEventListener("click", updateOutput9);
+    outputD9.textContent = localStorage.getItem("content9");
+    input_textarea9.value = localStorage.getItem("content9");
+
+    function updateOutput9() {
+      localStorage.setItem("content9", input_textarea9.value);
+
+      outputD9.textContent = input_textarea9.value;
     }
   }
